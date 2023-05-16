538efb8aceb66213daa721f2782bbf1e7c16a910
Yuji Yokoo <yujiyokoo@gmail.com>
2023-05-16 17:19:40 +0900

    Update for release-0.3.0-beta3-20230516

--- Casks/stash.rb
+++ Casks/stash.rb
@@ -2,13 +2,13 @@ cask "stash" do
   name "stash"
   homepage "https://github.com/cipherstash/cli-releases"
   desc "stash is the official command line utility for working with CipherStash"
-  version "0.3.0-beta2-20230331"
+  version "0.3.0-beta3-20230516"
   arch arm: "aarch64",
        intel: "x86_64"
   binary "stash-#{arch}-apple-darwin", target: "stash"
 
-  sha256 arm: "55e4c156afb07ee1bd4d77b23ce12e90cb7d22a53f112bd5711db4d585ca9df9",
-         intel: "64b99f9a9b66341b94f2678a84f2ecb3dc86f611227d4112933924c53eea14b9"
+  sha256 arm: "3e6e28b76fb42d2bbce1917ce4d7e097dd3723e2764b327be1918443e0c14bbb",
+         intel: "9832b0c1bc5bc79f63b524855650c9e0a821b06d699a461d60cbc56c5cdb0bd2"
 
   url "https://github.com/cipherstash/cli-releases/releases/download/release-#{version}/stash-#{arch}-apple-darwin"
 end
