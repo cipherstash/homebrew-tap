40ac4597547fad99ac7304ff36373f6ba3d20b03
Yuji Yokoo <yujiyokoo@gmail.com>
2024-05-03 16:29:30 +0900

    Update CLI to 1.0.0

--- Casks/stash.rb
+++ Casks/stash.rb
@@ -2,13 +2,13 @@ cask "stash" do
   name "stash"
   homepage "https://github.com/cipherstash/cli-releases"
   desc "stash is the official command line utility for working with CipherStash"
-  version "0.3.0-beta7-20231017"
+  version "1.0.0-20240503"
   arch arm: "aarch64",
        intel: "x86_64"
   binary "stash", target: "stash"
 
-  sha256 arm: "1d7b2f4a6134adf62ecf2b28b95106d33bf11e4edba6485be377a53d800971d8",
-         intel: "1cba02dbe3524d52f98bed81adbe6bac7bb9269dfc337a04be61e1781dde5a89"
+  sha256 arm: "914388a3e2b3dca55126c4b015ff333e21e8a46b3dfb98700bd784a64546f3ff",
+         intel: "bbce484a2d1d84d98ac79d7a7a384682f41b7377457d8cdd73a516c173d79954"
 
   url "https://github.com/cipherstash/cli-releases/releases/download/release-#{version}/stash-#{arch}-apple-darwin.dmg"
 end
